6275cd827e8e52d537ee89f6dde7aa1b4af5be69
lampard <thanh230497@gmail.com>
2020-02-13 14:58:37 +0700

    Update exercise 7: finish k-mean part

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,8 +21,15 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+m = size(X, 1);
 
-
+for i = 1:m
+  xi = X(i, :);
+  temp = centroids - xi;
+  temp = sum(temp.^2, 2);
+  [a, index] = min(temp);
+  idx(i) = index;
+end
 
 
 
